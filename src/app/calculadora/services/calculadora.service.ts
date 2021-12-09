8e4ee5b05d62ba1440580e18d04e1c50a9225182
David W de Jesus <davidwj15@gmail.com>
2021-12-09 20:11:52 -0300

    adicionado comentarios

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,10 @@
+/**
+ * Serviço responsável por executar as operações
+ * da calculadora
+ * 
+ * @autor David
+ * @since 1.0.0
+ */
 import { ClassField } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 
@@ -5,7 +12,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
-
+  /*
+   define as constantes utilizadas
+   para identificar as operações de cálculo
+   */
   static readonly SOMA: string = '+';
   static readonly SUBTRACAO: string = '-';
   static readonly DIVISAO: string = '/';
@@ -13,8 +23,16 @@ export class CalculadoraService {
 
   constructor() { }
 
+  /**
+   * 
+   * @param num1 number Primeiro numero digitado pelo usuario
+   * @param num2 number Segundo numero digitado pelo usuario
+   * @param operacao string Operação a ser executada
+   * @returns number Retorna resultado da operação
+   */
+
   calcular(num1: number, num2: number, operacao: string): number {
-    let resultado: number;
+    let resultado: number; // armazena o resultado da operação
     switch(operacao){
       case CalculadoraService.SOMA:
         resultado = num1 + num2;
