f91135e4a126c448066dc912aaf47e658d33ced0
David W de Jesus <davidwj15@gmail.com>
2021-12-13 11:09:07 -0300

    adicionado teste unitario de calculo de soma

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -7,7 +7,9 @@
  */
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable(
+  { providedIn: 'root' }
+)
 export class CalculadoraService {
   /*
    define as constantes utilizadas
